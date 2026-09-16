8703a52c6806a7e1641ed6949d3fef740d59b7f5
slamsarr <slamsarr@gmail.com>
2026-09-16 17:01:02 +0000

    Fix runtime: import useMemo manquant dans Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { T } from "../lib/calcul";
